8870edef9d9c45a5f86c92be975efc1a9db2d858
amazea5 <dweep.xp@gmail.com>
2024-05-31 12:17:07 +0600

    prints name or username

---
+++ names.py
@@ -0,0 +1,9 @@
+def names(n):
+    if n == "1":
+        return "Sadik Mina Dweep"
+    elif n == "2":
+        return "amazea5"
+
+
+print(f"Press 1 for my name.\nPress 2 for my username")
+print(names(input()))
