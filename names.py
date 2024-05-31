ecce705801440bc1470c3ce67fd7419ba6b73478
amazea5 <dweep.xp@gmail.com>
2024-05-31 14:51:23 +0600

    Handled invalid inputs

--- names.py
+++ names.py
@@ -1,9 +1,12 @@
-def names(n):
+def names():
+    n = input()
     if n == "1":
-        return "Sadik Mina Dweep"
+        print ("Sadik Mina Dweep")
     elif n == "2":
-        return "amazea5"
-
+        print ("amazea5")
+    else:
+    	print("Enter a valid option")
+    	names()
 
 print(f"Press 1 for my name.\nPress 2 for my username")
-print(names(input()))
+names()
